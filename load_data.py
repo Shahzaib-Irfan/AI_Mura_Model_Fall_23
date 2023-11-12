5a920f8eee57c4739ec58b45b588601d7d41eff4
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-11-12 10:28:15 +0500

    Implemented load Data

--- load_data.py
+++ load_data.py
@@ -16,3 +16,23 @@ class Data(object):
     def train_df(self):
         return pd.read_csv('../MURA-v1.1/train_image_paths.csv', header=None, 
                            names=['FilePath'])
+
+    
+    @property
+    def valid_df(self):
+        return pd.read_csv('../MURA-v1.1/valid_image_paths.csv', header=None, 
+                           names=['FilePath'])
+    
+    @property
+    def train_labels_data(self):
+        return pd.read_csv('../MURA-v1.1/train_labeled_studies.csv',
+                           names=['FilePath', 'Labels'])
+    
+    @property
+    def valid_labels_data(self):
+        return pd.read_csv('../MURA-v1.1/valid_labeled_studies.csv',
+                           names=['FilePath', 'Labels'])
+
+if __name__ == '__main__':
+    data = Data()
+    print(data.valid_labels_data)
