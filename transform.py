a116e19ece5252617402098a8dd60c8f7846c390
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-11-12 10:32:36 +0500

    Added Prototype of Transform Class

---
+++ transform.py
@@ -0,0 +1,9 @@
+from load_data import Data
+import warnings
+warnings.filterwarnings('ignore')
+
+class SingularDataframe(object):
+    """
+        Represents a joint instance of the studies, body parts, labels, image paths
+        in a singular dataframe.
+    """
