80c6db26b06c83ae443c8d243a370d1df410d770
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-11-12 10:33:33 +0500

    Merged CSV Data to be readable by the model

--- transform.py
+++ transform.py
@@ -7,3 +7,40 @@ class SingularDataframe(object):
         Represents a joint instance of the studies, body parts, labels, image paths
         in a singular dataframe.
     """
+
+    @property
+    def labeled_train(self):
+        """
+            Return a joint Singular Dataframe of both the valid and train dataframes.
+            
+            Returns:
+                valid_df : The dataframe of validation images.
+                train_df : The dataframe of training images.
+        """
+        
+        df = Data()
+        
+        train_df, valid_df = df.train_df, df.valid_df
+        train_labels_data = df.train_labels_data
+        valid_labels_data = df.valid_labels_data
+        
+        # print(train_df.head())
+
+        
+        train_df['Label'] = train_df.apply(lambda f: 1 if 'positive' in f.FilePath else 0, axis=1)
+        train_df['BodyPart'] = train_df.apply(lambda f: (f.FilePath.split('/')[2][3:]).lower(), axis=1)
+        train_df['StudyType'] = train_df.apply(lambda f: f.FilePath.split('/')[4][:6], axis=1)
+        #print(train_df.head(25))
+        
+        
+        valid_df['Label'] = valid_df.apply(lambda f: 1 if 'positive' in f.FilePath else 0, axis=1)
+        valid_df['BodyPart'] = valid_df.apply(lambda f: (f.FilePath.split('/')[2][3:]).lower(), axis=1)
+        valid_df['StudyType'] = valid_df.apply(lambda f: f.FilePath.split('/')[4][:6], axis=1)
+        
+        
+        train_df.set_index(["FilePath", "BodyPart"]).count(level="BodyPart")
+
+        train_df.set_index(["FilePath", "Label"]).count(level="Label")
+        
+        return valid_df, train_df
+        
